e390b159485de875cf2637a03c24465f04f1dc51
mcoronado <matias.coronado@usach.cl>
2020-11-06 23:34:45 -0300

    6

--- labAnalisis.R
+++ labAnalisis.R
@@ -5,7 +5,7 @@
 #https://www.kaggle.com/uciml/breast-cancer-wisconsin-data
 
 url.Inutil <- 'https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data'
-
+data.Inutil <- read.csv(url.Inutil)
 
 
 data <- read.csv(file.path("c:/Users/elmat/Desktop/LAB1Analisis", "data.csv"))
@@ -14,3 +14,24 @@ data <- read.csv(file.path("c:/Users/elmat/Desktop/LAB1Analisis", "data.csv"))
 data["X"] <- NULL
 
 head(data)
+
+"
+Pasos.
+1. Buscar significado de las variables
+2. Identificar relacion
+3. Analizar estadisticos de cada variable
+
+"
+
+
+"
+Variables:
+id: identificador del paciente
+diagnosis: diagnosis de los tejidos mamarios, en donde (M) es maligno y (B) benigno
+
+radius: 
+
+Por cada variable se calcula:
+Media/SE/Worst
+
+"
