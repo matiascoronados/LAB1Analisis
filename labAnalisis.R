b13d8976eb2a103afd54c4f19244c02b147bdf7b
mcoronado <matias.coronado@usach.cl>
2020-11-10 20:11:11 -0300

    corr + fix en datos

--- labAnalisis.R
+++ labAnalisis.R
@@ -1,30 +1,17 @@
 
 library(tidyverse)
-
-
+library(ggplot2)
+library(GGally)
+library(psych)
+library(corrplot)
 #PDF
 #https://www.researchgate.net/publication/311950799_Analysis_of_the_Wisconsin_Breast_Cancer_Dataset_and_Machine_Learning_for_Breast_Cancer_Detection/link/5864757e08ae329d6203aa82/download
 
 
 url_datos <- 'https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data'
 
-"
-
-     Group 1: 367 instances (January 1989)
-     Group 2:  70 instances (October 1989)
-     Group 3:  31 instances (February 1990)
-     Group 4:  17 instances (April 1990)
-     Group 5:  48 instances (August 1990)
-     Group 6:  49 instances (Updated January 1991)
-     Group 7:  31 instances (June 1991)
-     Group 8:  86 instances (November 1991)
-     -----------------------------------------
-     Total:   699 points (as of the donated datbase on 15 July 1992)
-  
-
-"
-
-data <- read.csv(url_datos)
+datos.duros <- read.csv(url_datos)
+data <- data.frame(datos.duros)
 
 colnames(data) <- c("id",
                     "clump.thickness",
@@ -38,38 +25,64 @@ colnames(data) <- c("id",
                     "mitoses",
                     "class")
 
-"
-falta eliminar los ?
-"
 
 
 
-"
-Atributos:
+bool.values <- data$bare.nuclei=='?'
+data <- data[!bool.values,]
+
+#Para confirmar que se eliminaron los ?
+sum(data$bare.nuclei=='?')
 
 
-   #  Attribute                     Domain
-   -- -----------------------------------------
-   1. Sample code number            identificador del paciente
-   2. Clump Thickness               1 - 10
-   3. Uniformity of Cell Size       1 - 10
-   4. Uniformity of Cell Shape      1 - 10
-   5. Marginal Adhesion             1 - 10
-   6. Single Epithelial Cell Size   1 - 10
-   7. Bare Nuclei                   1 - 10
-   8. Bland Chromatin               1 - 10
-   9. Normal Nucleoli               1 - 10
-  10. Mitoses                       1 - 10
-  11. Class:                        diagnosis de los tejidos mamarios, en donde (4) es maligno y (2) benigno.
+dd <- data
+
+#En el caso de que se quiera cambiar los 4 y 2, por M y B
+dd$class <- replace(dd$class,dd$class==2,'Benigno')
+dd$class <- replace(dd$class,dd$class==4,'Maligno')
+
+#Se aplica factor
+dd[["class"]] <- factor(dd[["class"]])
+data[["bare.nuclei"]] <- factor(data[["bare.nuclei"]])
 
 
-Cada parametro esta medido con una escala que varia del 1 al 10, siendo 1 el mas cercano a benigno, y 10 a maligno
-
-Del data-set original, se identificaron 9 variables que diferian signficiativamente en el resultao de los diagnosticos de los ejidos mamarios.
-
-Los datos son consideraciones noise-free (?)
-
-"
+#Se pasa a numerico los valores de bare nuclei.
+data$bare.nuclei <- as.numeric(data$bare.nuclei)
 
 
-sum(is.na(data))
+#Se elimina la columna ID
+#dd$id <- NULL
+data$id <- NULL
+
+
+#VISTA GENERAL
+ggpairs(data, aes(colour=class, alpha=0.4))
+
+
+#GRAFICO DE CORRELACION
+corrplot.mixed(cor(data),
+               lower = "number", 
+               upper = "circle",
+               tl.col = "black")
+
+
+#GRAFICO DE CAJAS
+
+library(ggpubr)
+library(cowplot)
+
+boxplot.width =  ggboxplot(data = data, x = "class", y = "width", color = "class") + border()
+
+
+#Grafico de cajas para clump.thickness
+
+boxplot.width =  ggboxplot(data = dd, x = "class", y = "clump.thickness", color = "class") + border()
+
+ydens = axis_canvas(boxplot.width, axis = "y", coord_flip = TRUE) + geom_density(data = dd, aes(x = clump.thickness, fill = class), alpha = 0.7, size = 0.2) + coord_flip()
+
+boxplot.width = insert_yaxis_grob(boxplot.width, ydens, grid::unit(.2, "null"), position = "right")
+
+ggdraw(boxplot.width)
+
+
+
